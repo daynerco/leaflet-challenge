32e709b82e7aa485ba38d15ac02934af4ca6e7c3
Dayna Cole <daynerco@gmail.com>
2024-04-11 19:49:03 -0400

    updated js code

--- Leaflet-Part-1/js/config.js
+++ Leaflet-Part-1/js/config.js
@@ -1,9 +1,2 @@
-// Store  API endpoint as queryURL
-var queryURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-
-
-// Use GET request to the query URL
-d3.json(queryURL).then(function(data){
-    // Once e get a response, send the data.features and data.features object to the createFeatures function.
-    createFeatures(data.features);
-  });
+// Store API key
+const API_KEY = ""
